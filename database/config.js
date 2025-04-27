b6b3abd6e418a2b1e3dd6cbb72913ebfc0680729
dipiCABJ <chichocat@gmail.com>
2025-04-27 09:08:07 -0300

    Final commit

--- database/config.js
+++ database/config.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.MONGODB_CNN);
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
         console.log('Database online!');
     } catch (err) {
